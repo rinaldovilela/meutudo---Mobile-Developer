51935125e7538155433df7a3c763379069adb0c5
rinaldovilela <rinaldoalvesvilela@hotmail.com>
2025-06-27 20:00:21 -0300

    feat: Adiciona o módulo de configuração e atualiza main.js para utilizar suas propriedades

--- Trabalhando com Node Modules/Modularização de Projetos Node.js com CommonJS/src/main.js
+++ Trabalhando com Node Modules/Modularização de Projetos Node.js com CommonJS/src/main.js
@@ -1,13 +1,20 @@
 const carrinho = require("./services/product.js");
+const config = require("./services/config.js");
 
 async function main() {
   console.log("carrinho de compras");
-  carrinho.getFullName("123", "Produto Exemplo");
-  carrinho.getFullName("456", "Outro Produto");
-  carrinho.getFullName("789", "Mais um Produto");
-  carrinho.getProductLabel("Produto Exemplo");
-  carrinho.getProductLabel("Outro Produto");
-  carrinho.getProductLabel("Mais um Produto");
+  //   carrinho.getFullName("123", "Produto Exemplo");
+  //   carrinho.getFullName("456", "Outro Produto");
+  //   carrinho.getFullName("789", "Mais um Produto");
+  //   carrinho.getProductLabel("Produto Exemplo");
+  //   carrinho.getProductLabel("Outro Produto");
+  //     carrinho.getProductLabel("Mais um Produto");
+  console.log(`Configuração:
+    Versão: ${config.version}   
+    Autor: ${config.author}
+    Descrição: ${config.description}`);
+
+  console.log("\n Dispositivo do cliente: " + config.client.device);
 }
 
 main();
---
+++ Trabalhando com Node Modules/Modularização de Projetos Node.js com CommonJS/src/services/config.js
@@ -0,0 +1,11 @@
+const devArea = {
+  version: "1.0.0",
+  author: "Rinaldo Vilela",
+  description: "Área de desenvolvimento do carrinho de compras",
+};
+
+const client = {
+  device: "mobile",
+};
+
+module.exports = { devArea, client };
