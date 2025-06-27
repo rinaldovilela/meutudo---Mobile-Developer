7d7d267b030dc2a7db1aeeae1661e74f9fc900fc
rinaldovilela <rinaldoalvesvilela@hotmail.com>
2025-06-27 20:31:11 -0300

    feat: Adiciona exportsDefault em funções para conectar e desconectar do banco de dados.

---
+++ Trabalhando com Node Modules/Modularização de Projetos Node.js com CommonJS/src/services/database.js
@@ -0,0 +1,7 @@
+exports.connectToDatabase = async (dataName) => {
+  console.log("se conectando ao banco de dados...");
+};
+
+exports.disconnectFromDatabase = async () => {
+  console.log("desconectando do banco de dados...");
+};
